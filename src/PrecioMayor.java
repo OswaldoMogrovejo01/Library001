50ceb400f396ef66c6d2de7623cdfefaf32ea797
Dbastida <bastidaszd@gmail.com>
2022-02-11 01:28:12 -0500

    subir precio mayor

--- src/PrecioMayor.java
+++ src/PrecioMayor.java
@@ -2,12 +2,12 @@ import java.util.Comparator;
 public class PrecioMayor implements Comparator<Libro> {
 	@Override
 	public int compare(Libro l1, Libro l2) {
-		if(l1.getPrecio()>l2.getPrecio()) {
-			return -1;
-		}
 		if(l1.getPrecio()<l2.getPrecio()) {
 			return 1;
 		}
 		else {
 			return 0;}}
+		if(l1.getPrecio()>l2.getPrecio()) {
+			return -1;
+		}
 }
