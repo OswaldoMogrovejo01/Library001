77f805ebb3aef358bd3ef0b399aa1a09dc8b5cce
Dbastida <bastidaszd@gmail.com>
2022-02-11 00:32:33 -0500

    Agrego el menu de opciones en el main

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	
 	private static Scanner leer = new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("\n***MENU DE OPCIONES***\n");
+		System.out.println("\n***jjjjMENU DE OPCIONES***\n");
 		System.out.println("<1> INGRESAR LIBRO\n"+"<2> BUSCAR LIBRO POR ISNB\n"+
 		"<3> VENTA DE LIBRO\n"+"<4> MOSTRAR LOS TRES LIBROS MAS CAROS\n"+
 		"<5> MOSTRAR TODOS LOS LIBROS\n"+"<6> SALIR DEL MENU");
---
+++ src/Main1.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main1 {
+	private static ArrayList<Libro> libros = new ArrayList<Libro>();
+	private static ArrayList<Libro> librosAltos = new ArrayList<Libro>();
+	
+	private static Scanner leer = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println("\n***MENU DE OPCIONES***\n");
+		System.out.println("<1> INGRESAR LIBRO\n"+"<2> BUSCAR LIBRO POR ISNB\n"+
+		"<3> VENTA DE LIBRO\n"+"<4> MOSTRAR LOS TRES LIBROS MAS CAROS\n"+
+		"<5> MOSTRAR TODOS LOS LIBROS\n"+"<6> SALIR DEL MENU");
+		menu();
+	}
+
+}
