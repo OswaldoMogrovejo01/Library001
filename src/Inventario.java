c32dc6776130f4bde51096620a079cfc84064958
Dbastida <bastidaszd@gmail.com>
2022-02-11 01:23:58 -0500

    Se carga Inventario

--- src/Inventario.java
+++ src/Inventario.java
@@ -3,12 +3,13 @@ import java.util.Collections;
 
 public class Inventario {
 	private ArrayList<Libro> libros = new ArrayList<Libro>();
-	public void almacenar(Libro libro) {
-		libros.add(libro);}
-	
 	public String getLibros() {
 		for(int i=0; i<libros.size(); i++) {
 			System.out.println(libros.get(i));}
 		return null;
 	}
+	public void almacenar(Libro libro) {
+		libros.add(libro);}
+	
+	
 }
